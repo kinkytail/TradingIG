6659c4be27da4b73e4c0705cce78c3af69f74d7e
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-04-16 22:06:32 +0200

    Getprices Fix

--- examples/getPricesPerDay.php
+++ examples/getPricesPerDay.php
@@ -3,9 +3,9 @@ include_once "../vendor/autoload.php";
 include_once "credentials.php";
 
 use League\Csv\Writer;
+
 $igApi = new Twinsen\TradingIG\Api();
 $loginResult = $igApi->getSecurityToken($login, $password, $apiKey);
-
 
 
 $igApi = new Twinsen\TradingIG\Api();
@@ -18,11 +18,15 @@ $endDate->modify('+ 1day');
 $csv = Writer::createFromFileObject(new SplTempFileObject());
 
 $prices = $igApi->getPrices("IX.D.DAX.IFD.IP", $startDate, $endDate);
-foreach($prices as $priceData){
-    $time = $priceData["snapshotTime"];
-    $closePrice = $priceData["closePrice"]["bid"];
+foreach ($prices as $priceData) {
+    $time = $priceData["snapshotTimeUTC"];
+    $time = DateTime::createFromFormat("Y-m-d\TH:i:s", $time);
+    $time = $time->format("d.m.Y H:i:s");
 
-    $csv->insertOne([$time,$closePrice]);
+    $closePrice = $priceData["closePrice"]["bid"];
+    $closePrice = str_replace(".",",",$closePrice);
+
+    $csv->insertOne([$time, $closePrice]);
 }
 
-file_put_contents('dax.csv',$csv->__toString());
+file_put_contents('dax.csv', $csv->__toString());
