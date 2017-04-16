199ba4fceb101f48e95b2b46ffad06d4e817a5c0
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-04-16 23:19:36 +0200

    Enumeration

---
+++ examples/enumerateMarkets.php
@@ -0,0 +1,23 @@
+<?php
+include_once "../vendor/autoload.php";
+include_once "credentials.php";
+
+use League\Csv\Writer;
+
+$igApi = new Twinsen\TradingIG\Api();
+
+if (!$igApi->login($apiKey, $accessToken, $accountId)) {
+    $accessToken = $igApi->getSecurityToken($login, $password, $apiKey);
+
+    $igApi->login($apiKey, $accessToken, $accountId);
+}
+
+
+$csv = Writer::createFromFileObject(new SplTempFileObject());
+
+$prices = $igApi->getAllMarkets("600346");
+foreach ($prices as $market) {
+    $csv->insertOne($market);
+}
+
+//file_put_contents('dax.csv', $csv->__toString());
--- src/Api.php
+++ src/Api.php
@@ -45,7 +45,8 @@ class Api
         $command = $this->client->getCommand('Login', array('identifier' => $login, 'password' => $password, "apiKey" => $apiKey));
         $command->set("command.headers", array("Content-type" => "application/json"));
         $responseModel = $this->client->execute($command);
-        return $responseModel;
+        $securityToken = $responseModel["oauthToken"]["access_token"];
+        return $securityToken;
     }
 
     public function login($apiKey, $securityToken, $accountId)
@@ -53,6 +54,20 @@ class Api
         $this->securityToken = $securityToken;
         $this->apiKey = $apiKey;
         $this->accountId = $accountId;
+        $command = $this->createCommand('checkSession',array());
+        $retVal = true;
+        try {
+            $responseModel = $this->client->execute($command);
+        } catch (\Guzzle\Http\Exception\ClientErrorResponseException $clientErrorResponseException) {
+            if ($clientErrorResponseException->getResponse()->getStatusCode() == 401) {
+                $retVal = false;
+            } else {
+                throw $clientErrorResponseException;
+            }
+        }
+        return $retVal;
+
+
     }
 
 
@@ -65,14 +80,7 @@ class Api
     {
 
 
-        $command = $this->client->getCommand('GetPrices', array('epic' => $epic, 'startDate' => $this->formatDate($startDate), "endDate" => $this->formatDate($endDate)));
-        $command->set("command.headers", array(
-            "X-IG-API-KEY" => $this->apiKey,
-            "IG-ACCOUNT-ID" => $this->accountId,
-            "Authorization" => "Bearer " . $this->securityToken,
-            "Version" => "3"
-        ));
-
+        $command = $this->createCommand('GetPrices', array('epic' => $epic, 'startDate' => $this->formatDate($startDate), "endDate" => $this->formatDate($endDate)));
         $iterator = $this->client->getIterator($command);
         $retArray = array();
         foreach ($iterator as $responseModel) {
@@ -81,6 +89,51 @@ class Api
 
         //$responseModel = $this->client->execute($command);
         //echo $this->debugCommand($command);
+        return $retArray;
+    }
+
+    public function createCommand($commandName, $commandArray)
+    {
+        $command = $this->client->getCommand($commandName, $commandArray);
+        $command->set("command.headers", array(
+            "X-IG-API-KEY" => $this->apiKey,
+            "IG-ACCOUNT-ID" => $this->accountId,
+            "Authorization" => "Bearer " . $this->securityToken,
+            //"Version" => "3"
+        ));
+        return $command;
+    }
+
+    public function getMarkets($hierarchyId)
+    {
+        $command = $this->createCommand('GetMarkets', array("hierarchyid" => $hierarchyId));
+        //$command->set("command.headers", array(
+        //    "Version" => "1"
+        //));
+        $result = $command->execute();
+        return $result;
+    }
+
+    public function getAllMarkets($hierarchyId)
+    {
+        $retArray = array();
+        $stack = new \SplStack();
+        do {
+
+            $result = $this->getMarkets($hierarchyId);
+            foreach ($result["nodes"] as $node) {
+                $stack->push($node["id"]);
+            }
+            if($result["markets"]){
+                $retArray = array_merge($retArray, $result["markets"]);
+            }
+            sleep(2);
+
+
+
+        } while ($stack->count());
+
+
         return $retArray;
     }
 
